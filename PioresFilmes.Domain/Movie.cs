b9263dccb7212aaa50e9731f2e3e9e9704843acd
Rafael Santos <rafael_dos_santos_25@hotmail.com>
2023-04-03 10:37:57 -0300

    Adicionar arquivos de projeto.

---
+++ PioresFilmes.Domain/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace PioresFilmes.Domain
+{
+    public class Movie
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public IEnumerable<Producer> Producers { get; set; }
+        public int Year { get; set; }
+        public bool Winner { get; set; }
+    }
+}
---
+++ PioresFilmes.Domain/Producer.cs
@@ -0,0 +1,8 @@
+﻿namespace PioresFilmes.Domain
+{
+    public class Producer
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
