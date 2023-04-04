1a96a7289fd243fac08461d4f554d9c79c419fe4
Rafael Santos <rafael_dos_santos_25@hotmail.com>
2023-04-04 15:35:12 -0300

    Criado controller

---
+++ PioresFilmes.Application/Controllers/ProducersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PioresFilmes.Application.Dto;
+using PioresFilmes.Application.Interfaces;
+
+namespace PioresFilmes.Application.Controllers
+{
+    public class FindProducersController : ControllerBase
+    {
+        private readonly IFindProducersService _findProducersService;
+
+        public FindProducersController(IFindProducersService findProducersService)
+        {
+            _findProducersService = findProducersService;
+        }
+
+        [HttpGet("/producers/intervals")]
+        public async Task<ActionResult<FindProducersDto>> FindProducerIntervalsAsync()
+        {
+            var result = await _findProducersService.FindProducerIntervalsAsync();
+            return Ok(result);
+        }
+    }
+}
--- PioresFilmes/Program.cs
+++ PioresFilmes/Program.cs
@@ -5,19 +5,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
 
-services.AddControllers();
-services.AddRouting(options => options.LowercaseUrls = true);
-
 services.AddDatabase();    
 services.AddApplicationServices();
 
+services.AddControllers();
+services.AddRouting(options => options.LowercaseUrls = true);
+
 var app = builder.Build();
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
 var initService = app.Services.CreateScope()
                      .ServiceProvider
                      .GetRequiredService<IInitializeDataService>();
 
 await initService.Initialize();
-
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
